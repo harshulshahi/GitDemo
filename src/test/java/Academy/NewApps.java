ba64a925a1595526612a4ba6c98415819af93e6f
harshulshahi <harshul_90@hotmail.com>
2020-10-26 15:30:44 -0400

    Arhitect Asian Development 9d9e129
    cd

---
+++ src/test/java/Academy/NewApps.java
@@ -0,0 +1,16 @@
+package Academy;
+
+import org.testng.annotations.AfterTest;
+
+public class NewApps {
+
+	
+	@AfterTest
+	public void teardown()
+	{
+	System.out.println("i have created another one to ");
+	System.out.println("changes are done  now again ");
+		
+	}
+	
+}
